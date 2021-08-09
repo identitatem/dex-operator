7599d33e98e792bb598c8eff85916c6c0102c11a
Chris Doan <cdoan@redhat.com>
2021-08-09 16:44:21 -0500

    apply CR design to api
    * add samples for both DexServer and DexCient

--- api/v1alpha1/dexserver_types.go
+++ api/v1alpha1/dexserver_types.go
@@ -20,22 +20,93 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
-// EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
-// NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
+// StaticPasswordSpec allows us to define login credentials. Do not expect us to use this.
+type StaticPasswordSpec struct {
+	Email string `json:"email,omitempty"`
+}
+
+// StorageSpec defines how/if we persist the configuration to a database on store in K8s.
+type StorageSpec struct {
+	Type string `json:"type,omitempty"`
+}
+
+// WebSpec defines override for cert to dex server.
+type WebSpec struct {
+	Http    string `json:"http,omitempty"`
+	Https   string `json:"https,omitempty"`
+	TlsCert string `json:"tlsCert,omitempty"`
+	TlsKey  string `json:"tlsKey,omitempty"`
+}
+
+// GrpcSpec defines override options on how we run grpc server. Addr should not need to change. The certs are required.
+type GrpcSpec struct {
+	Addr        string `json:"addr,omitempty"`
+	TlsCert     string `json:"tlsCert,omitempty"`
+	TlsKey      string `json:"tlsKey,omitempty"`
+	TlsClientCA string `json:"tlsClientCA,omitempty"`
+}
+
+// ExpirySpec defines how we expire
+type ExpirySpec struct {
+	DeviceRequests string `json:"deviceRequests,omitempty"`
+}
+
+// LoggerSpec defines loggingoptions. Optional
+type LoggerSpec struct {
+	Level  string `json:"level,omitempty"`
+	Format string `json:"format,omitempty"`
+}
+
+// Oauth2Spec defines dex behavior flags
+type Oauth2Spec struct {
+	ResponseTypes         []string `json:"responseTypes,omitempty"`
+	SkipApprovalScreen    bool     `json:"skipApprovalScreen,omitempty"`
+	AlwaysShowLoginScreen bool     `json:"alwaysShowLoginScreen,omitempty"`
+	PasswordConnector     string   `json:"passwordConnector,omitempty"`
+}
+
+// ConfigSpec describes the client id and secret. The RedirectURI should be returned?
+type ConfigSpec struct {
+	ClientID        string `json:"clientID,omitempty"`
+	ClientSecretRef string `json:"clientSecretRef,omitempty"`
+	// TODO: confirm if we set this, or allow this to be passed in?
+	RedirectURI string `json:"redirectURI,omitempty"`
+	Org         string `json:"org,omitempty"`
+}
+
+// ConnectorSepc defines the OIDC connector config details
+type ConnectorSpec struct {
+	Name   string     `json:"name,omitempty"`
+	Type   string     `json:"type,omitempty"`
+	Id     string     `json:"id,omitempty"`
+	Config ConfigSpec `json:"config,omitempty"`
+}
 
 // DexServerSpec defines the desired state of DexServer
 type DexServerSpec struct {
 	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
 	// Important: Run "make" to regenerate code after modifying this file
-
 	// Foo is an example field of DexServer. Edit dexserver_types.go to remove/update
 	Foo string `json:"foo,omitempty"`
+	// TODO: Issuer references the dex instance web URI. Should this be returned as status?
+	Issuer           string               `json:"issuer,omitempty"`
+	EnablePasswordDB bool                 `json:"enablepassworddb,omitempty"`
+	StaticPasswords  []StaticPasswordSpec `json:"staticpasswords,omitempty"`
+	Storage          StorageSpec          `json:"storage,omitempty"`
+	Web              WebSpec              `json:"web,omitempty"`
+	Grpc             GrpcSpec             `json:"grpc,omitempty"`
+	Expiry           ExpirySpec           `json:"expiry,omitempty"`
+	Logger           LoggerSpec           `json:"logger,omitempty"`
+	Oauth2           Oauth2Spec           `json:"oauth2,omitempty"`
+	Connectors       []ConnectorSpec      `json:"connectors,omitempty"`
 }
 
 // DexServerStatus defines the observed state of DexServer
 type DexServerStatus struct {
-	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
-	// Important: Run "make" to regenerate code after modifying this file
+	// +optional
+	State string `json:"state,omitempty"`
+	// +optional
+	Message string `json:"message,omitempty"`
 }
 
 //+kubebuilder:object:root=true
